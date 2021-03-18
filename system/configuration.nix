0b05c3d87597852c727294da926098ce30721e1f
jkimmerling <80171344+jkimmerling@users.noreply.github.com>
2021-03-18 09:57:07 -0400

    Update configuration.nix

--- system/configuration.nix
+++ system/configuration.nix
@@ -24,7 +24,14 @@ in
       # Window manager 
       ./wm/xmonad.nix
     ];
-
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/nvme0n1"; # or "nodev" for efi only
   networking = {
     # Enables wireless support and openvpn via network manager.
     networkmanager = {
